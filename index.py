8cf014a25ab84a578572a86070e101d0012351d3
PAULO HENRIQUE RICARDO DA SILVA <pauloxzv322@gmail.com>
2026-04-29 21:45:56 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,106 @@
+# contador = 1
+
+# while contador <=10:
+#     print(contador)
+#     if contador == 5:
+#         break
+#     contador +=1
+
+# senha = ""
+# while True:
+#     senha = input("Digite a senha:")
+
+#     if senha == "1234":
+#         print("Acesso liberado")
+#         break
+#     print("senha incorreta")
+
+# ####continue
+# numero = 1
+# while numero <= 5:
+#     if numero ==2:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+
+# contador = 0
+# while contador < 10:
+#      contador+=1
+#      if contador % 2 ==0:
+#          continue
+
+
+# ###Atividade
+# ###Questão 01
+# numero = 1
+# while numero <= 10:
+#     print(numero)
+
+#     if numero == 6:
+#         break
+#     numero +- 1
+    
+
+    
+ ###Questão 02
+
+# numero = 1
+# while numero <= 10:
+#     if numero == 5:
+#         numero += 1
+#     continue
+# print(numero)
+# numero+= 1
+
+
+# ###Questão 03
+# numero = 1
+# while numero <= 20:
+#     if numero == 15:
+#         break
+#     if numero % 2 == 0:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+
+# ###Questão 04
+# while True:
+#     produto = input("Digite o nome do produto:")
+
+#     if produto == "fim":
+#         print("cadastro finalizado")
+#         break
+#     print("Produto Cadastrado, produto")
+
+# ###Questao 05
+# soma = 0
+# while True:
+#     numero = int(input("Digite um numero: "))
+#     soma += numero
+
+#     if soma >=20:
+#         break
+# print("Total",soma )
+
+# ###Questão 06
+# soma = 0
+# while True:
+#     numero = int(input("Digite um numero"))
+#     soma += numero
+
+#     if soma >= 50:
+#         print("Limite atingido!")
+#         print("Total:", soma)
+#         break
+
+# ###Questao 07
+# while True:
+#     senha = input("Digite a senha:")
+
+#     if senha == "teste":
+#         print("Acesso liberado")
+#         break
+#     else:
+#         print("senha incorreta")
